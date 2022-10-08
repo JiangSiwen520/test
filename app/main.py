da1e204fa1d83834eaca77214ba7a66f77451149
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2022-10-08 19:07:59 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Welcome to CloudBase!\n'
+    return 'Hello, Welcome to CloudBase_JSW!!!\n'
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
