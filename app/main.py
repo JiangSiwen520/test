30ac996bedf9075006b1c5d5cbe74760b11e4ae5
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-01 20:04:25 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,7 +9,8 @@ def hello_world():
     import pandas as pd
     df = pd.read_excel('000001.xlsx')
     a = df.columns[0]
-    return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!\n'+a
+    b = 'date'
+    return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!->5.1\n'+b+a
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
