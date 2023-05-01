14905b5756da2df53260f0a81ee4be90f77615cd
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-01 19:59:21 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!\n'
+    import pandas as pd
+    df = pd.read_excel('000001.xlsx')
+    a = df.columns[0]
+    return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!\n'+a
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
