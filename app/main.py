297ce25bfe1d7bff3e51df18ed62d35c7ae3f846
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 13:21:29 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -25,6 +25,6 @@ def fun2():
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=8080)
 
 
