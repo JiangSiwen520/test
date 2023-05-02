a6098a25f75446718af426f55652e63fa5affacf
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 12:46:53 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,23 @@ import os
 
 from flask import Flask
 import model
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller # ADF单位根检验
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Welcome to CloudBase!\n'
+
+@app.route('/ADF')
+def hello_world():
+    df = pd.read_excel('000001.xlsx')
+    ## 平稳性检验
+    result = adfuller(df[series].dropna()) #不能拒绝原假设，即原序列存在单位根
+    p = result[1].round(4)
+     
+    return str(p)
 
 # 将 model 模块里的蓝图对象 new_list 注册到 app
 app.register_blueprint(model.ADF)
