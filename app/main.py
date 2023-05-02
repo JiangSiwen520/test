4dbbf83a7eba4bb4ee96a4aeb3573b26a463da93
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 14:24:34 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,26 @@
 import os
 
 from flask import Flask
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller # ADF单位根检验
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Welcome to CloudBase!\n'
+    return 'Hello, Welcome to CloudBase!35\n'
+@app.route('/ADF')
+def fun2():
+    
+    df = pd.read_excel('000001.xlsx')
+    result = adfuller(df['price'].dropna()) #不能拒绝原假设，即原序列存在单位根
+    p = result[1].round(4)
+    q = 4
+    return {
+        'p':p
+        }
+
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='0.0.0.0', debug=False, port=2000)
