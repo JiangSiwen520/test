de577e62ab3f2cadbf6ad84b69baa31b3b95f41b
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 12:03:49 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, Welcome to CloudBase!\n'
 
+# 将 model 模块里的蓝图对象 new_list 注册到 app
+app.register_blueprint(model.ADF)
+
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
     app.run(host='0.0.0.0', debug=True, port=80)
