e45f4fb037e00d22cd0f12cd8a30495cc9bdbd00
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 13:51:45 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,23 +1,13 @@
 import os
 
 from flask import Flask
-import model
-import pandas as pd
-from statsmodels.tsa.stattools import adfuller # ADF单位根检验
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Welcome to CloudBase!13:4\n'
-
-
-
-# 将 model 模块里的蓝图对象 new_list 注册到 app
-# app.register_blueprint(model.ADF)
+    return 'Hello, Welcome to CloudBase!\n'
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
     app.run(host='0.0.0.0', debug=True, port=80)
-
-
