835d089cd5df464dba6ef509089d436643c0aebf
JiangSiwen520 <115335073+JiangSiwen520@users.noreply.github.com>
2023-05-02 11:20:47 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 import os
 
 from flask import Flask
-import model
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, Welcome to CloudBase_JSW, 2023-02-13!!!->2023.5.1\n'
+    return 'Hello, Welcome to CloudBase!\n'
 
 if __name__ == "__main__":
     #app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='0.0.0.0', debug=True, port=80)
